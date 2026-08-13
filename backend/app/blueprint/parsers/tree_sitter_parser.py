90c4c0645c67efa164378c7e408489210727b2ea
nguyenduybaohlqn-bit <nguyenduybaohlqn@gmail.com>
2026-08-13 14:26:30 +0700

    add tree_sitter_parser

--- backend/app/blueprint/parsers/tree_sitter_parser.py
+++ backend/app/blueprint/parsers/tree_sitter_parser.py
@@ -1,5 +1,10 @@
 from tree_sitter import Parser
 from tree_sitter_language_pack import get_language
 
-parser = Parser()
-parser.language = get_language("python")
+
+
+def parse_file(file_path, language : str):
+    parser = Parser()
+    parser.language = get_language(language)
+    code = file_path.read_bytes()
+    return parser.parse(code)
